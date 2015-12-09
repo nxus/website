9a7f244cefb16deedee62174822033ed53a5e3ac
Mike Reich <mjreich@gmail.com>
2015-12-08 18:37:20 -0800

    First commit with latest nxus modules

---
+++ index.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: Mike Reich
+* @Date:   2015-12-08 16:42:44
+* @Last Modified 2015-12-08
+*/
+
+'use strict';
+
+
+var App = require('@nxus/core').Application
+
+var app = new App();
+
+app.start()
+
+module.exports = app; 
